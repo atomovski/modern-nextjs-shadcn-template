d329cb6f9f165a6ce91910a2a2a80b0a6f2332f0
Aleks Tomovski <atomovski@gmail.com>
2025-08-20 15:03:47 +1000

    CORS update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,27 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async headers() {
+   return [
+     {
+       source: '/:path*',
+       headers: [
+         {
+           key: 'Cross-Origin-Resource-Policy',
+           value: 'cross-origin'
+         },
+         {
+           key: 'Cross-Origin-Embedder-Policy', 
+           value: 'require-corp'
+         },
+         {
+           key: 'Cross-Origin-Opener-Policy',
+           value: 'unsafe-none'
+         }
+       ]
+     }
+   ]
+ },
 };
 
 module.exports = nextConfig;
